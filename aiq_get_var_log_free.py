ddb2c9a87f8477300f514e1fd7e830a51348c69e
Scott Chubb <schubb@gmail.com>
2021-03-11 11:29:50 -0500

    pep8 syntax updates

--- aiq_get_var_log_free.py
+++ aiq_get_var_log_free.py
@@ -6,7 +6,9 @@
 #   tested in lab situations, all use cases cannot be accounted for.
 """
 
-import sys, time, os
+import sys
+import time
+import os
 import requests
 from datetime import datetime
 from prettytable import PrettyTable
@@ -24,43 +26,13 @@ def build_payload():
     return payload
 
 
-#def build_events(response_dict, search_string):
-#    """
-#    Build the events list
-#    """
-#    constant_list = []
-#    constant_dict_pre = {}
-#    for key,val in response_dict.items():
-#        cluster_name = key
-#        response_json = val
-#        if search_string is not None:
-#            for cls_constant in search_string.split(","):
-#                for dict_key in response_json.keys():
-#                    if cls_constant in dict_key.lower():
-#                        constant_list.append(dict_key)
-#                        constant_out = response_json[dict_key]
-#                        cls_key = cluster_name + "_" + dict_key
-#                        constant_dict_pre[cls_key] = constant_out
-#            if len(constant_list) == 0:
-#                print(f"\t{cls_constant} not found")
-#                sys.exit(1)
-#        elif search_string is None:
-#            for dict_key in response_json.keys():
-#                    constant_out = response_json[dict_key]
-#                    cls_key = cluster_name + "-" + dict_key
-#                    constant_dict_pre[cls_key] = constant_out
-#        else:
-#            sys.exit(1)
-#    return constant_dict_pre
-
-
 def build_output(outfile_name, **constant_dict):
     out_table = PrettyTable()
-    out_table.field_names = ["Cluster","Constant Name", "Constant Setting"]
-    #out_table.max_width['Constant Setting'] = 60
+    out_table.field_names = ["Cluster", "Constant Name", "Constant Setting"]
+    # out_table.max_width['Constant Setting'] = 60
     out_table.align["Constant Name"] = "l"
     out_table.align["Constant Setting"] = "l"
-    for key,val in constant_dict.items():
+    for key, val in constant_dict.items():
         cls_name = key.split("-")[0]
         cnst_name = key.split("-")[1]
         cnst_out = val
@@ -68,7 +40,7 @@ def build_output(outfile_name, **constant_dict):
     out_table_text = out_table.get_string()
     print(out_table)
     with open("./output_files/" + outfile_name, "a") as out_file:
-        out_file.write(out_table_text + "\n")   
+        out_file.write(out_table_text + "\n")
 
 
 def get_filename(cluster_name):
@@ -96,16 +68,16 @@ def parse_response(fs_dict):
         hdr5 = "Percent Free"
         print(f"{hdr1:>11}{hdr2:>11}{hdr3:>11}{hdr4:>11}{hdr5:>14}")
         for nodes in fs_val['nodes']:
-            #print(nodes)
             node_id = nodes['nodeID']
             for mount_entry in nodes['result']['mountEntries']:
                 if mount_entry['directory'] == '/var/log':
-                    fs_size = round((mount_entry['capacity']/1024/1024/1024),2)
-                    fs_avail = round((mount_entry['available']/1024/1024/1024),2)
-                    fs_free = round((mount_entry['free']/1024/1024/1024),2)
-                    pct_free = round((fs_free/fs_size*100),2)
+                    fs_size = round((mount_entry['capacity']/1073741824), 2)
+                    fs_avail = round((mount_entry['available']/1073741824), 2)
+                    fs_free = round((mount_entry['free']/1073741824), 2)
+                    pct_free = round((fs_free/fs_size*100), 2)
                     print(f"{node_id:>11}{fs_size:>10}G{fs_avail:>10}G"
                           f"{fs_free:>10}G{pct_free:>13}%\n")
+
 
 def main():
     """
@@ -115,12 +87,14 @@ def main():
     auth_cookie = web_login(user, user_pass)
     payload = build_payload()
     headers = build_headers(auth_cookie)
-    response_json = build_connect(headers,payload)
+    response_json = build_connect(headers, payload)
     customer_dict = parse_customer(response_json, search_customer)
     cluster_dict = parse_cluster(customer_dict, search_cluster)
-    fs_dict = build_response(headers, cluster_dict, search_string, api_call='ListMountedFileSystems', node_id=None)
+    fs_dict = build_response(headers, cluster_dict,
+                             search_string, api_call='ListMountedFileSystems',
+                             node_id=None)
     parse_response(fs_dict)
-    #build_output(outfile_name, **constant_dict)
+    # build_output(outfile_name, **constant_dict)
 
 if __name__ == "__main__":
     main()
